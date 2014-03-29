5e411f82e80e3e63f0b68b05f3cd61d447e843b9
Chris Nain <chrisnain@hotmail.com>
2014-03-29 20:56:16 +0100

    Adding action bar.

--- Around/src/de/chrisnain/around/DisplayMessageActivity.java
+++ Around/src/de/chrisnain/around/DisplayMessageActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
@@ -26,15 +27,16 @@ public class DisplayMessageActivity extends ActionBarActivity {
 		textView.setTextSize(40);
 		textView.setText(message);
 		
-		setContentView(textView);
+		this.getActionBar().setDisplayHomeAsUpEnabled(true);
+		
+		this.setContentView(textView);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.display_message, menu);
-		return true;
+	    MenuInflater inflater = getMenuInflater();
+	    inflater.inflate(R.menu.main_activity_actions, menu);
+		return super.onCreateOptionsMenu(menu);
 	}
 
 	@Override
